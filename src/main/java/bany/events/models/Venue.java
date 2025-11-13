8758be0287822c90439fbf33f14c82913f6f545f
bany <bpuertaherrera@gmail.com>
2025-11-13 12:18:32 -0500

    feat: create models

---
+++ src/main/java/bany/events/models/Venue.java
@@ -0,0 +1,15 @@
+package bany.events.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Venue {
+    Long id;
+    String name;
+    String location;
+}
+
