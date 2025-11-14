5b4898c9f2ff7a99231f6bcd6ab145c0217d7da7
bany-ph <bpuertaherrera@gmail.com>
2025-11-13 22:30:55 -0500

    feat: implement VenueMapper and update EventMapper to use it

--- src/main/java/bany/events/mappers/EventMapper.java
+++ src/main/java/bany/events/mappers/EventMapper.java
@@ -2,16 +2,19 @@ package bany.events.mappers;
 
 import bany.events.dtos.request.EventRequest;
 import bany.events.dtos.response.EventResponse;
+
 import bany.events.models.Event;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = VenueMapper.class)
 public interface EventMapper {
 
-    EventMapper EVENTMAPPER = Mappers.getMapper(EventMapper.class);
+    EventMapper EVENT_INSTANCE = Mappers.getMapper(EventMapper.class);
 
+    @Mapping(source = "idVenue", target = "idVenue")
     EventResponse toResponseDto(Event event);
 
     @Mapping(target = "id", ignore = true)
